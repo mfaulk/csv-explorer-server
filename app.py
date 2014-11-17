555f574829f6e56583d5520ee561231a6b4551df
Matt Faulkner <mnfaulk@gmail.com>
2014-11-17 14:05:41 -0800

    fixed typo

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def allowed_file(filename):
 
 from database import db_session
 
-@app.teardown_appcontex
+@app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
