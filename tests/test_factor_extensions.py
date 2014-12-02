047772121661df13ce8f586c6373756d32a5c950
Matt Faulkner <mnfaulk@gmail.com>
2014-12-01 18:34:15 -0800

    refactoring

--- tests/test_factor_extensions.py
+++ tests/test_factor_extensions.py
@@ -1,28 +1,31 @@
 import unittest
-from framework import ext
+from framework import package_contents, subclasses_in_module, subclasses_in_package, get_factor_extensions
 from test_extensions import sample_factor
 from factors.nodes import FactorNode
 
 class TestModels(unittest.TestCase):
 
     def test_extensions(self):
-        modules, pathname = ext.package_contents('test_extensions')
+        modules, pathname = package_contents('test_extensions')
         self.assertTrue('__init__' in modules)
         self.assertTrue('sample_factor' in modules)
 
     def test_factor_subclasses(self):
-        klasses = ext.subclasses_in_module(FactorNode, sample_factor)
+        klasses = subclasses_in_module(FactorNode, sample_factor)
         klass_names = [klass.__name__ for klass in klasses]
         self.assertTrue("SampleFactor" in klass_names)
 
     def test_subfactors(self):
-        klasses = ext.subclasses_in_package(FactorNode, 'test_extensions')
+        klasses = subclasses_in_package(FactorNode, 'test_extensions')
         klass_names = [klass.__name__ for klass in klasses]
         self.assertTrue("SampleFactor" in klass_names)
         self.assertTrue("AnotherFactor" in klass_names)
         self.assertFalse("FactorNode" in klass_names)
         # if klass.__doc__:
         #     print klass.__doc__
+
+    def test_get_factor_info(self):
+        get_factor_extensions()
 
 if __name__ == '__main__':
     unittest.main()
